a6277ba92695baa5b5cd9580604df339015cb528
Scott Byrne <91495085+sby051@users.noreply.github.com>
2022-12-16 14:11:51 +0000

    updated

--- linux/assets/config.fish
+++ linux/assets/config.fish
@@ -1,3 +1,25 @@
+# Environment variables
+
+# fish
+set -gx FISH_CONFIG_FILE = "$FISH_CONFIG_DIR/config.fish"
+# fish end
+
+# flutter
+set -gx FLUTTER_DIR "/opt/flutter/bin"
+set -gx PATH "$FLUTTER_DIR" $PATH
+# flutter end
+
+# rust / cargo
+set -gx CARGO_DIR "/opt/cargo/bin"
+set -gx PATH "$CARGO_DIR" $PATH
+# rust / cargo end
+
+# cmake
+set -gx CMAKE_DIR "/opt/cmake/bin"
+set -gx PATH "$CMAKE_DIR" $PATH
+# cmake end
+
+# Aliases
 alias apt="aptitude"
 alias apt-uu="sudo apt update && sudo apt upgrade -y"
 alias apt-uuu="sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y"
@@ -22,7 +44,7 @@ alias .4="cd ../../../.."
 alias .5="cd ../../../../.."
 alias md="mkdir"
 alias edit-alias="nano ~/.config/fish/config.fish"
-alias update-alias="source ~/.config/fish/config.fish"
+alias update-alias="source ~/.config/fish/config.fish && cp ~/.config/fish/config.fish ~/repos/configs/linux/assets"
 alias python="python3.11"
 alias py="python"
 alias pip="pip3"
@@ -30,17 +52,7 @@ alias nmap!="nmap -v -sC -sV -oN nmap.log $1"
 alias cat="batcat"
 alias gobuster!="gobuster dir -u $1 -w ~/repos/pentesting/tools/wordlists/directory/directory-list.txt -o gobuster.log"
 
-starship init fish | source
+# Startup commands
 nvm use latest
 cd
 clear
-
-# flutter
-set -gx FLUTTER_DIR "/opt/flutter/bin"
-set -gx PATH "$FLUTTER_DIR" $PATH
-# flutter end
-
-# rust / cargo
-set -gx CARGO_DIR "/opt/cargo/bin"
-set -gx PATH "$CARGO_DIR" $PATH
-# rust / cargo end
