6a5d92bcb68ce8e93efac9a38d0454506f319b7c
sby051 <91495085+sby051@users.noreply.github.com>
2022-12-18 23:32:01 +0000

    update

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing initial dependencies..."
-sudo apt update -y -qq
-sudo apt upgrade -y -qq
-sudo apt install python3.11 -y -qq
-sudo apt install python3-pip -y -qq
+sudo apt update -y -qq -o Dpkg::Use-Pty=0
+sudo apt upgrade -y -qq -o Dpkg::Use-Pty=0
+sudo apt install python3.11 -y -qq -o Dpkg::Use-Pty=0
+sudo apt install python3-pip -y -qq -o Dpkg::Use-Pty=0
 mkdir -p $HOME/repos
 cd $HOME/repos
 git clone https://github.com/sby051/configs.git
