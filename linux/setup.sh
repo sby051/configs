479668d34804fc579bb6091554e2064bd1a21268
sby051 <91495085+sby051@users.noreply.github.com>
2023-01-04 14:47:49 +0000

    changed

--- linux/setup.sh
+++ linux/setup.sh
@@ -5,30 +5,24 @@ echo "Installing initial dependencies..."
 sudo apt update -y -qq
 sudo apt upgrade -y -qq
 
-# remove any existing python installs
-sudo apt remove python3.* -y -qq
-sudo apt remove python3-pip -y -qq
-
-# install python 3.11
-sudo apt install software-properties-common -y -qq
+# install python3
+echo "Installing python3..."
 sudo add-apt-repository ppa:deadsnakes/ppa -y -qq
-sudo apt update -y -qq
-sudo apt install python3.11 -y -qq
-
-# create a symlink for python3, python and pip3
-sudo ln -s $(which python3.11) /usr/bin/python3
-sudo ln -s $(which python3.11) /usr/bin/python
-sudo ln -s $(which pip3) /usr/local/bin/pip
+sudo apt install python3 -y -qq
+sudo apt install python3-pip -y -qq
 
 # install curl and git
+echo "Installing curl and git..."
 sudo apt install curl -y -qq
 sudo apt install git -y -qq
 
 # prepare the system for the installation of the configs
+echo "Preparing the system for the installation of the configs..."
 mkdir -p $HOME/repos
 cd $HOME/repos
 git clone https://github.com/sby051/configs.git
 cd configs/linux
 
 # install the configs
-python main.py
+echo "Installing the configs..."
+python3 main.py
